a8fe7ece79009e2402bb291599e4a1281247418c
itlemon <lemon_jiang@aliyun.com>
2020-07-21 23:12:52 +0800

    [title] Spring Boot源码分析工程初始化
    description: Spring Boot源码分析工程初始化

---
+++ spring-boot-sample/src/main/java/cn/itlemon/spring/boot/sample/SpringBootSampleApplication.java
@@ -0,0 +1,17 @@
+package cn.itlemon.spring.boot.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jiangpingping
+ * @date 2020-07-21 23:05
+ */
+@SpringBootApplication
+public class SpringBootSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSampleApplication.class, args);
+    }
+
+}
---
+++ spring-boot-sample/src/test/java/cn/itlemon/spring/boot/sample/SpringBootSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.itlemon.spring.boot.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
